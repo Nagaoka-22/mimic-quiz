5948cc25826494bb1bc7a0851fa0af9c5cdd1a20
Nagaoka-22 <nagaoka.y.22@gmail.com>
2023-07-29 18:00:35 +0900

    利用規約、プライバシーポリシー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'rooms#show'
 
+  get '/terms' => 'static_pages#terms'
+  get '/privacy_policy' => 'static_pages#privacy_policy'
+  
   devise_for :users
   get 'rooms/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
