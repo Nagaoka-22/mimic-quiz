0e569bb56ed4360c022222dc547ea32579f5b116
Nagaoka-22 <nagaoka.y.22@gmail.com>
2023-09-13 21:24:55 +0900

    ゲストユーザーのログイン・編集

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,10 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
 end
